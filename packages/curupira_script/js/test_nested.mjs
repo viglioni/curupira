474174b8b2043048d226041a12dc52a414ee5e90
laura <lviglioni@canarytechnologies.com>
2025-12-01 17:18:16 -0300

    chore: remove test file

--- packages/curupira_script/js/test_nested.mjs
+++
@@ -1,7 +0,0 @@
-import Fixtures from './elixir_bundle.js';
-
-console.log('Testing nested bundle...');
-console.log('Fixtures:', Fixtures);
-console.log('Fixtures.Simple:', Fixtures.Simple);
-console.log('Fixtures.Simple.hello():', Fixtures.Simple.hello());
-console.log('Fixtures.WithArgs.greet("World"):', Fixtures.WithArgs.greet("World"));
